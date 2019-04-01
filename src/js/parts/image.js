5d8e2f68abe5d64ddbdfa7e2a106a196230eecc1
Victor <justicejesus1237@gmail.com>
2019-04-01 19:50:05 +0300

    image

--- src/js/parts/image.js
+++ src/js/parts/image.js
@@ -1,6 +1,28 @@
 function image() {
+	let imgPopup = document.createElement('div'),
+		image = document.createElement('img'),
+		imgLink = document.querySelectorAll('.img-link');
 
+	imgPopup.classList.add('img-popup');
+	image.classList.add('img-content');
+	document.body.appendChild(imgPopup);
+	imgPopup.appendChild(image);
+
+	imgLink.forEach(item => {
+		item.addEventListener('click', function (event) {
+			event.preventDefault();
+			imgPopup.style.display = 'flex';
+			image.src = this.href;
+		});
+	});
+
+	imgPopup.addEventListener('click', event => {
+		if (event.target && event.target.classList.contains('img-popup')) {
+			imgPopup.style.display = 'none';
+		}
+	});
 }
 
 
-export default images;
+
+export default image;
